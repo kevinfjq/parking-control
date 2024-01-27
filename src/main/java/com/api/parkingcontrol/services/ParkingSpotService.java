017bbe8ebd834b6a5b69141bcdf85e293ed21714
kevinfjq <kevifjq@gmail.com>
2024-01-27 20:28:08 -0300

    Add endpoint to delete parking spot by ID

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -40,4 +40,8 @@ public class ParkingSpotService {
     public Optional<ParkingSpotModel> findById(UUID id) {
         return parkingSpotRepository.findById(id);
     }
+    @Transactional
+    public void delete(ParkingSpotModel parkingSpotModel) {
+        parkingSpotRepository.delete(parkingSpotModel);
+    }
 }
